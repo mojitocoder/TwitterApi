1c6ce075efe0c58494d6a7700c7e5230b5e9e20f
Quynh Nguyen <quynhnh@gmail.com>
2015-11-05 13:46:59 +0000

    Using Buffer over the stream

--- Streaming/Program.cs
+++ Streaming/Program.cs
@@ -8,6 +8,7 @@ using Tweetinvi;
 using Tweetinvi.Core.Credentials;
 using Tweetinvi.Core.Enum;
 using Tweetinvi.Core.Events.EventArguments;
+using Tweetinvi.Core.Interfaces;
 using Tweetinvi.Core.Interfaces.Streaminvi.Parameters;
 
 namespace Streaming
@@ -21,10 +22,10 @@ namespace Streaming
             // *********************
             // Credentials
             // *********************
-            string consumerKey = "";
-            string consumerSecret = "";
-            string accessToken = "";
-            string accessTokenSecret = "";
+            var consumerKey = "1IkRIDMoGhwD0uqvoUq1vxtcF";
+            var consumerSecret = "xybThYiZs5nViaoXtOQ19Thvp7xf5n0w2vII8Lj93gfX7QMu8f";
+            var accessToken = "391233365-ud5RMbWDF4agMjIo63F3ZEsTocpADSH7Tycj0K67";
+            var accessTokenSecret = "F4C6LHt0B1YAxCTTtmm2d5zfZgcENmRrPeNGsKbCoedoY";
 
             // *********************
             // 3 ways to use credentials
@@ -52,12 +53,15 @@ namespace Streaming
             //    Console.WriteLine(arguments.Tweet);
             //};
 
-            Observable.FromEventPattern<TweetReceivedEventArgs>(stream, "TweetReceived").Subscribe(foo =>
-            {
-                var tweet = foo.EventArgs.Tweet;
+            Observable.FromEventPattern<TweetReceivedEventArgs>(stream, "TweetReceived")
+                .Select(foo => foo.EventArgs.Tweet) //trim off all the unnecessary elements from each element (i.e. event) in the stream
+                .Buffer(TimeSpan.FromSeconds(3))
+                .Subscribe(tweets =>
+                {
+                    var authors = string.Join(", ", tweets.Select(foo => foo.CreatedBy));
 
-                Console.WriteLine(tweet.CreatedBy);
-            });
+                    Console.WriteLine("\n\n{0}: {1} tweets from: {2}", DateTime.UtcNow, tweets.Count, authors);
+                });
 
             stream.StartStream();
 
@@ -66,9 +70,9 @@ namespace Streaming
             //    Console.WriteLine("Tick");
             //});
 
-
-
             Console.ReadLine();
+
+            stream.StopStream();
         }
     }
 }
